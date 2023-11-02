5cc261ca74e0832966f887a851bbb095ced46ef1
LucasF <che.fernandez255@gmail.com>
2023-11-01 22:44:33 -0600

    created models for user and thoughts and schema

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trimmed: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+        },
+        thoughts: {
+            type: Schema.Types.ObjectId,
+            ref: 'thought'
+        },
+        friends: {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
