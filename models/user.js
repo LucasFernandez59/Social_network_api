a77890911e76ab6135bfdee137751f162e73ea4c
LucasF <che.fernandez255@gmail.com>
2023-11-05 00:25:50 -0600

    fixed syntax

--- models/user.js
+++ models/user.js
@@ -14,14 +14,21 @@ const userSchema = new Schema(
             unique: true,
             match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
         },
-        thoughts: {
-            type: Schema.Types.ObjectId,
+        thoughts: [ 
+            {type: Schema.Types.ObjectId,
             ref: 'thought'
+        }],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
         },
-        friends: {
-            type: Schema.Types.ObjectId,
-            ref: 'user'
-        }
+        id: false
     }
 );
 
